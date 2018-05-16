7e5f46a4cdd5ac3e5108f547b0e29f0902dd87e5
Brad Gibson <b2b@humanenginuity.com>
2018-05-16 07:56:28 -0700

    CaptchaSolver|SumWhenMatchesNext()|no digits: green (2m)

--- day_01/src/unit_tests/captcha_solver/sum_when_matches_next.rs
+++ day_01/src/unit_tests/captcha_solver/sum_when_matches_next.rs
@@ -18,6 +18,17 @@ impl Env {
 #[test]
 fn tests() {
     run(&given("an initialized CaptchaSolver", Env::new(), |ctx| {
+        ctx.when("fed nothing", |ctx| {
+            ctx.before(|env| {
+                env.result = env.solver.sum_when_matches_next("");
+            });
+            let expected_result = 0;
+
+            ctx.then("the result should be 0", move |env| {
+                assert!(env.result == expected_result);
+            });
+        });
+
         ctx.when("fed a single digit: 1", |ctx| {
             ctx.before(|env| {
                 env.result = env.solver.sum_when_matches_next("1");
